64fb8f6486062025e935a5313b5674272ff7b83d
Daniel Prastiwa <daniel.prastiwa@akupeduli.org>
2022-02-08 12:57:20 +0700

    create new project

---
+++ Mim Generator/Features/ListImages/Data/Meme.swift
@@ -0,0 +1,8 @@
+//
+//  Meme.swift
+//  Mim Generator
+//
+//  Created by Daniel Prastiwa on 08/02/22.
+//
+
+import Foundation
---
+++ Mim Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mim Generator
+//
+//  Created by Daniel Prastiwa on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
